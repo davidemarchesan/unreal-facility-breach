a7c291f84acc173743d5a5ffeec6783ef254ac6c
davidemarchesan <marchesan.davide.code@gmail.com>
2025-12-11 22:09:51 +0100

    Dash tuning

--- Source/FacilityBreach/Pawns/Movement/FirstPersonMovementComponent.cpp
+++ Source/FacilityBreach/Pawns/Movement/FirstPersonMovementComponent.cpp
@@ -3,29 +3,97 @@
 
 #include "FirstPersonMovementComponent.h"
 
-void UFirstPersonMovementComponent::DoDash()
+void UFirstPersonMovementComponent::PhysCustom(float deltaTime, int32 Iterations)
 {
+	Super::PhysCustom(deltaTime, Iterations);
 
-	if (IsFalling())
+	if (deltaTime < MIN_TICK_TIME)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("isfalling, no dash"));
 		return;
 	}
 
-	// Get current direction
+	// Only custom mode right now
+	PhysDashing(deltaTime, Iterations);
+}
+
+void UFirstPersonMovementComponent::PhysDashing(float deltaTime, int32 Iterations)
+{
+	if (deltaTime < MIN_TICK_TIME)
+	{
+		return;
+	}
+
+	RestorePreAdditiveRootMotionVelocity();
+
+	if (bStartDash == false)
+	{
+		DashRemainingTime = DashDuration;
+		bStartDash = true;
+	}
+	
+	FVector AdjustedVelocity = Velocity * deltaTime;
+	FHitResult Hit(1.f);
+	
+	FVector TargetVelocity = AdjustedVelocity;
+	TargetVelocity.Normalize();
+	TargetVelocity = TargetVelocity * MaxWalkSpeed * deltaTime;
+
+	float t = DashRemainingTime / DashDuration;
+
+	AdjustedVelocity = FMath::InterpEaseIn(TargetVelocity, AdjustedVelocity, t, 2.f);
+	
+	SafeMoveUpdatedComponent(AdjustedVelocity, UpdatedComponent->GetComponentQuat(), true, Hit);
+
+	if (Hit.Time < 1.f)
+	{
+		// We hit something, dash is over
+		bStartDash = false;
+		
+		SetMovementMode(MOVE_Walking);
+		return;
+	}
+	
+	DashRemainingTime -= deltaTime;
+	if (DashRemainingTime <= 0.f)
+	{
+		// Dash is over
+		bStartDash = false;
+
+		// Keep walking at max speed
+		// and let walking mode handle it
+		AdjustedVelocity.Normalize(); // Direction of the dash
+		Velocity = AdjustedVelocity * MaxWalkSpeed;
+		
+		SetMovementMode(MOVE_Walking);
+		return;
+	}
+}
+
+void UFirstPersonMovementComponent::DoDash()
+{
+
+	// Get current movement (not facing) direction
 	FVector VelocityDirection = Velocity;
 	VelocityDirection.Z = 0.0f; // No jump
 	
 	if (VelocityDirection.IsNearlyZero())
 	{
+		// Dash forward if idle
 		VelocityDirection = UpdatedComponent->GetForwardVector();
 	}
 	
 	VelocityDirection.Normalize();
-
-	 UE_LOG(LogTemp, Warning, TEXT("Velocity Direction %f %f %f"), VelocityDirection.X, VelocityDirection.Y, VelocityDirection.Z);
-
-	// Dash velocity
-	Velocity = VelocityDirection * DashVelocity;
 	
+	// Dash velocity
+	Velocity = VelocityDirection * DashVelocity * DashVelocityScalar;
+
+	SetMovementMode(MOVE_Custom, static_cast<uint8>(ECustomMovementMode::MOVE_Dashing));
+	
+}
+
+bool UFirstPersonMovementComponent::IsDashing()
+{
+	return MovementMode == MOVE_Custom
+		&& CustomMovementMode == static_cast<uint8>(ECustomMovementMode::MOVE_Dashing)
+		&& UpdatedComponent;
 }
--- Source/FacilityBreach/Pawns/Movement/FirstPersonMovementComponent.h
+++ Source/FacilityBreach/Pawns/Movement/FirstPersonMovementComponent.h
@@ -6,6 +6,12 @@
 #include "GameFramework/CharacterMovementComponent.h"
 #include "FirstPersonMovementComponent.generated.h"
 
+enum class ECustomMovementMode : uint8
+{
+	MOVE_Dashing,
+};
+
+
 /**
  * 
  */
@@ -14,12 +20,34 @@ class FACILITYBREACH_API UFirstPersonMovementComponent : public UCharacterMoveme
 {
 	GENERATED_BODY()
 
+protected:
+
+	// Custom physics
+	virtual void PhysCustom(float deltaTime, int32 Iterations) override;
+
+	virtual void PhysDashing(float deltaTime, int32 Iterations);
+
 public:
 
 	// Dash
-	UPROPERTY(Category="Character Movement: Dashing", EditAnywhere, BlueprintReadWrite, meta=(DisplayName="Dash Velocity", ClampMin="0", UIMin="0", ForceUnits="cm/s"))
+	UPROPERTY(Category="Character Movement: Dashing", EditAnywhere, BlueprintReadWrite, meta=(DisplayName="Dash Velocity", ClampMin="0", UIMin="0"))
 	float DashVelocity;
+
+	UPROPERTY(Category="Character Movement: Dashing", EditAnywhere, BlueprintReadWrite, meta=(DisplayName="Dash Duration", ClampMin="0", UIMin="0", ForceUnits="s"))
+	float DashDuration;
+
+private:
+
+	float DashRemainingTime = 0.f;
+
+	float DashVelocityScalar = 1000.f;
+
+	bool bStartDash = false;
+
+public:
 	
 	void DoDash();
+
+	virtual bool IsDashing();
 	
 };
