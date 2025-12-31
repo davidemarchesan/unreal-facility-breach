0c8e12851eefeadf66f16961715231d70ea86c4f
davidemarchesan <marchesan.davide.code@gmail.com>
2025-12-31 12:57:54 +0100

    Change keycard mesh

---
+++ Source/FacilityBreach/Actors/PickupItems/KeyCard.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PickupItem.h"
+#include "KeyCard.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FACILITYBREACH_API AKeyCard : public APickupItem
+{
+	GENERATED_BODY()
+	
+};
